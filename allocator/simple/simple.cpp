f2f41e6ea64ed98e289f8fe1f38672727ac8463b
gilgil <gilgil@gilgil.net>
2015-05-14 19:14:32 +0900

    Add MyAllocator class

--- allocator/simple/simple.cpp
+++ allocator/simple/simple.cpp
@@ -1,7 +1,22 @@
 #include <memory>
 
-int main() {
+void simple() {
   std::allocator<char> a;
   std::allocator<char>::pointer p = a.allocate(1234);
   a.deallocate(p, 1234);
+}
+
+template<typename _Tp>
+class MyAllocator : public std::allocator<_Tp> {
+};
+
+void inherit() {
+  MyAllocator<char> a;
+  std::allocator<char>::pointer p = a.allocate(5678);
+  a.deallocate(p, 5678);
+}
+
+int main() {
+  simple();
+  inherit();
 }
