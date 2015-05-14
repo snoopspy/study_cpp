62e174abc746d31d4e62e9e8874a60089bcac3bd
gilgil <gilgil@gilgil.net>
2015-05-14 19:35:04 +0900

    Revert "Add MyAllocator class"
    This reverts commit f2f41e6ea64ed98e289f8fe1f38672727ac8463b.

--- allocator/simple/simple.cpp
+++ allocator/simple/simple.cpp
@@ -1,22 +1,7 @@
 #include <memory>
 
-void simple() {
+int main() {
   std::allocator<char> a;
   std::allocator<char>::pointer p = a.allocate(1234);
   a.deallocate(p, 1234);
-}
-
-template<typename _Tp>
-class MyAllocator : public std::allocator<_Tp> {
-};
-
-void inherit() {
-  MyAllocator<char> a;
-  std::allocator<char>::pointer p = a.allocate(5678);
-  a.deallocate(p, 5678);
-}
-
-int main() {
-  simple();
-  inherit();
 }
