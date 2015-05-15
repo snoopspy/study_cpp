0e34822b84ba73c406034c180866a50eda51d2c7
gilgil <gilgil@gilgil.net>
2015-05-15 12:57:18 +0900

    Add map_test

--- allocator/myallocator/myallocator_test.cpp
+++ allocator/myallocator/myallocator_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <map>
 #include "myallocator.h"
 
 void simple_test() {
@@ -9,24 +10,28 @@ void simple_test() {
 }
 
 void string_test() {
-  std::string s;
-  typedef std::basic_string<char, std::char_traits<char>, MyAllocator<char> > MyString;
-  MyString ms;
+  std::basic_string<char, std::char_traits<char>, MyAllocator<char> > ms;
   ms = "hello world";
   std::cout << ms << std::endl;
 }
 
 struct Obj { char buf[256]; };
 void list_test() {
-  //std::list<int> intList;
-  std::list<Obj, MyAllocator<Obj> > myIntList;
-  Obj obj;
-  myIntList.push_back(obj);
-  myIntList.push_back(obj);
+  std::list<Obj, MyAllocator<Obj> > myList;
+  myList.push_back(Obj());
+  myList.push_back(Obj());
+}
+
+void map_test() {
+  std::map<int, Obj, std::less<int>, MyAllocator<std::pair<int, Obj>>> myMap;
+
+  myMap[0] = Obj();
+  myMap[1] = Obj();
 }
 
 int main() {
   //simple_test();
   //string_test();
-  list_test();
+  //list_test();
+  map_test();
 }
